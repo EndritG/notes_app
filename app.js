3c720e8a6c530924af2e8b00083ff0260650862b
unknown <endrit.germizaj2@gmail.com>
2022-02-25 10:42:00 +0100

    test-Validator

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 
 // const sum = add(4, -2);
 // console.log(sum);
-
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
 console.log(msg);
+
+console.log(validator.isEmail('endritg18@gmail.com'));
+console.log(validator.isURL('https.med.io'));

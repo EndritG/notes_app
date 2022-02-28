db6d3d2e843b3193afc12b08ed627dfb8489cda0
unknown <endrit.germizaj2@gmail.com>
2022-02-28 14:54:55 +0100

    explore yargs

--- app.js
+++ app.js
@@ -3,13 +3,24 @@
 // const sum = add(4, -2);
 // console.log(sum);
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
-console.log(msg);
+// const msg = getNotes();
+// console.log(msg);cls
 
-const greenMsg = chalk.red.inverse.bold('Error!');
-console.log(greenMsg);
+// const greenMsg = chalk.red.inverse.bold('Error!');
+// console.log(greenMsg);
 
-// console.log(validator.isEmail('endritg18@gmail.com'));
-// console.log(validator.isURL('https.med.io'));
+// // console.log(validator.isEmail('endritg18@gmail.com'));
+// // console.log(validator.isURL('https.med.io'));
+
+// console.log(process.argv);
+
+console.log(yargs.argv);
+
+// if (command === 'add') {
+//   console.log('Adding Note!');
+// } else if (command === 'remove') {
+//   console.log('Removing Note!');
+// }
